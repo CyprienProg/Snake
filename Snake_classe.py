3878d460085358f6896498af34fea2dacfd3cf64
CyprienProg <cyprien@skynet.be>
2015-04-13 14:11:10 +0200

    Nourriture avalée

--- Snake_classe.py
+++ Snake_classe.py
@@ -1,8 +1,10 @@
 ﻿# Créé par Cyprien, le 11/12/2014 en Python 3.2
 from tkinter import *
+from random import *
+
 
 class GUI:
-    def __init__(self, hauteur, largeur):
+    def __init__(self, largeur, hauteur):
         self.fenetre = Tk()
         self.__hauteur = hauteur
         self.__largeur =largeur
@@ -10,12 +12,16 @@ class GUI:
         self.canvas.pack(padx = 10, pady = 10)
         self.var = "Down"
 
-        self.nourriture = Nourriture(self.canvas, 100, 100, 10)
-        self.serpent = Serpent(self.canvas)
+        self.map = Map(self.canvas, 31, 23, 25)
+
+        self.nourriture = Nourriture(self.canvas, self.map, 100, 100, 10)
+        self.serpent = Serpent(self.canvas, self.map)
         self.fenetre.bind("<Left>", self.go_left)
         self.fenetre.bind("<Right>", self.go_right)
         self.fenetre.bind("<Up>", self.go_up)
         self.fenetre.bind("<Down>", self.go_down)
+        self.nourriture.nouvellePosition()
+
 
     def go_left(self, event):
         if self.var != "Right" and self.lock == False and self.var != "Left":
@@ -65,134 +71,207 @@ class GUI:
 
     def movement(self):
         self.move()
-        self.fenetre.after(500, self.movement)
+        self.checkcollision()
+        self.fenetre.after(100, self.movement)
 
     def run(self):
-        self.fenetre.after(500, self.movement)
+        self.fenetre.after(100, self.movement)
         self.fenetre.mainloop()
 
+    def checkcollision(self):
+        pos_serpent = self.serpent.getposition()
+        pos_nourriture = self.nourriture.getposition()
+        if pos_serpent == pos_nourriture:
+            self.nourriture.nouvellePosition()
+
+
 class Corps:
-    def __init__(self, canvas, pos_x, pos_y, taille, couleur = "green"):
+    def __init__(self, canvas, map, pos_x, pos_y, taille, couleur = "green"):
         self.taille = taille
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.canvas = canvas
+        self.map = map
         self.couleur = couleur
-        self.id = self.canvas.create_rectangle(self.pos_x, self.pos_y, self.pos_x+self.taille, self.pos_y+self.taille, fill = self.couleur)
+        self.id = self.canvas.create_rectangle(0, 0, self.taille, self.taille, fill = self.couleur)
+        self.setposition(pos_x, pos_y)
 
     def move_left(self):
-        self.pos_x -= self.taille
+        self.pos_x -= 1
         self.update()
 
     def move_right(self):
-        self.pos_x += self.taille
+        self.pos_x += 1
         self.update()
 
     def move_up(self):
-        self.pos_y -= self.taille
+        self.pos_y -= 1
         self.update()
 
     def move_down(self):
-        self.pos_y += self.taille
+        self.pos_y += 1
         self.update()
 
-    def get_x(self):
-        return self.pos_x
+    def getposition(self):
+        return (self.pos_x, self.pos_y)
 
-    def get_y(self):
-        return self.pos_y
-
-    def set_x(self, x):
-        self.pos_x = x
-        self.update()
-
-    def set_y(self, y):
-        self.pos_y = y
+    def setposition(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
         self.update()
 
     def update(self):
-        self.canvas.coords(self.id, self.pos_x, self.pos_y, self.pos_x+self.taille, self.pos_y+self.taille)
+        self.map.updateposition(self.pos_x, self.pos_y, self.id, self.taille)
 
 class Serpent:
     taille = 25
-    pos_x = 50
-    pos_y = 50
-    def __init__(self, canvas):
+    pos_x = 0
+    pos_y = 0
+    def __init__(self, canvas, map):
         self.liste = []
         self.canvas = canvas
-        for i in range (10):
-            corps = Corps(self.canvas, self.pos_x + self.taille*i, self.pos_y, self.taille)
+        self.map = map
+        for i in range (3):
+            corps = Corps(self.canvas, self.map, self.pos_x + i, self.pos_y, self.taille)
             self.liste.append(corps)
 
     def go_left(self):
         dernier_corps = self.liste.pop()
         self.liste.insert(0, dernier_corps)
-        x = self.liste[1].get_x() - self.taille
-        y = self.liste[1].get_y()
-        dernier_corps.set_x( x )
-        dernier_corps.set_y( y )
-
+        pos = self.liste[1].getposition()
+        pos = (pos[0]-1, pos[1])
+        dernier_corps.setposition(pos[0], pos[1])
+        self.pos_x = pos[0]
+        self.pos_y = pos[1]
 
     def go_right(self):
         dernier_corps = self.liste.pop()
         self.liste.insert(0, dernier_corps)
-        x = self.liste[1].get_x() + self.taille
-        y = self.liste[1].get_y()
-        dernier_corps.set_x( x )
-        dernier_corps.set_y( y )
+        pos = self.liste[1].getposition()
+        pos = (pos[0]+1, pos[1])
+        dernier_corps.setposition(pos[0], pos[1])
+        self.pos_x = pos[0]
+        self.pos_y = pos[1]
 
     def go_up(self):
         dernier_corps = self.liste.pop()
         self.liste.insert(0, dernier_corps)
-        x = self.liste[1].get_x()
-        y = self.liste[1].get_y() - self.taille
-        dernier_corps.set_x( x )
-        dernier_corps.set_y( y )
+        pos = self.liste[1].getposition()
+        pos = (pos[0], pos[1]-1)
+        dernier_corps.setposition(pos[0], pos[1])
+        self.pos_x = pos[0]
+        self.pos_y = pos[1]
 
     def go_down(self):
         dernier_corps = self.liste.pop()
         self.liste.insert(0, dernier_corps)
-        x = self.liste[1].get_x()
-        y = self.liste[1].get_y() + self.taille
-        dernier_corps.set_x( x )
-        dernier_corps.set_y( y )
+        pos = self.liste[1].getposition()
+        pos = (pos[0], pos[1]+1)
+        dernier_corps.setposition(pos[0], pos[1])
+        self.pos_x = pos[0]
+        self.pos_y = pos[1]
 
     def possible_left(self):
-        pos = (self.liste[0].get_x() - self.taille, self.liste[0].get_y())
+        pos = self.liste[0].getposition()
+        pos = (pos[0]-1, pos[1])
         for corps in self.liste:
-            if pos[0] == corps.get_x() and pos[1] == corps.get_y() :
+            pos_corps = corps.getposition()
+            if pos[0] == pos_corps[0] and pos[1] == pos_corps[1] :
                 return False
         return True
 
     def possible_right(self):
-            pos = (self.liste[0].get_x() + self.taille, self.liste[0].get_y())
-            for corps in self.liste:
-                if pos[0] == corps.get_x() and pos[1] == corps.get_y() :
-                    return False
-            return True
+        pos = self.liste[0].getposition()
+        pos = (pos[0]+1, pos[1])
+        for corps in self.liste:
+            pos_corps = corps.getposition()
+            if pos[0] == pos_corps[0] and pos[1] == pos_corps[1] :
+                return False
+        return True
 
     def possible_up(self):
-        pos = (self.liste[0].get_x(), self.liste[0].get_y() - self.taille)
+        pos = self.liste[0].getposition()
+        pos = (pos[0], pos[1]-1 )
         for corps in self.liste:
-            if pos[0] == corps.get_x() and pos[1] == corps.get_y() :
+            pos_corps = corps.getposition()
+            if pos[0] == pos_corps[0] and pos[1] == pos_corps[1] :
                 return False
         return True
 
     def possible_down(self):
-        pos = (self.liste[0].get_x(), self.liste[0].get_y() + self.taille)
+        pos = self.liste[0].getposition()
+        pos = (pos[0], pos[1]+1 )
         for corps in self.liste:
-            if pos[0] == corps.get_x() and pos[1] == corps.get_y() :
+            pos_corps = corps.getposition()
+            if pos[0] == pos_corps[0] and pos[1] == pos_corps[1] :
                 return False
         return True
 
+    def getposition(self):
+        return (self.pos_x, self.pos_y)
+
 class Nourriture:
-    def __init__(self, canvas, pos_x, pos_y, taille, couleur = "white"):
+    def __init__(self, canvas, map, pos_x, pos_y, taille, couleur = "white"):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.couleur = couleur
         self.taille = taille
         self.canvas = canvas
-        self.id = self.canvas.create_oval(self.pos_x, self.pos_y, self.pos_x+self.taille, self.pos_y+self.taille, fill = self.couleur)
+        self.map = map
+        self.id = self.canvas.create_oval(0, 0, self.taille, self.taille, fill = self.couleur)
+        self.setposition(pos_x,pos_y)
+
+    def nouvellePosition(self):
+        x = randint(0, self.map.getlongueur())
+        y = randint(0, self.map.getlargeur())
+        self.setposition(x,y)
+
+    def getposition(self):
+        return (self.pos_x, self.pos_y)
+
+    def setposition(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.map.updateposition(self.pos_x, self.pos_y, self.id, self.taille)
+
+class Map:
+    def __init__(self, canvas, longueur, largeur, taille):
+        self.canvas = canvas
+        self.longueur = longueur
+        self.largeur = largeur
+        self.map =[]
+        self.taille = taille
+        self.setsize(longueur, largeur)
+
+
+    def setsize(self, longueur, largeur):
+        self.longueur = longueur
+        self.largeur = largeur
+        self.map = []
+        for y in range(self.largeur):
+            self.map.append([])
+            for x in range(self.longueur):
+                self.map[y].append(0)
+
+    def afficher(self):
+        for x in range(len(self.map)):
+            for y in range(len(self.map[x])):
+                print(self.map[x][y], end=" ")
+            print()
+
+    def updateposition (self, pos_x, pos_y, id, taille):
+        x0 = pos_x * self.taille + self.taille/2.0 - taille/2.0
+        y0 = pos_y * self.taille + self.taille/2.0 - taille/2.0
+        x1 = pos_x * self.taille + taille + self.taille/2.0 - taille/2.0
+        y1 = pos_y * self.taille + taille + self.taille/2.0 - taille/2.0
+        self.canvas.coords(id, x0, y0, x1, y1)
+
+    def getlargeur(self):
+        return self.largeur
+
+    def getlongueur(self):
+        return self.longueur
+
 
 
 
